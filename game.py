52645bac0a06fb516ed8fa9707593e1928e2ae9e
michael <zzval741@gmail.com>
2021-03-28 14:38:18 +0300

    add comments, refactoring

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+# импорт нужных библиотек
 import os
 import sys
 
@@ -5,10 +6,12 @@ import pygame
 import random
 from pygame import mixer
 
+# переменные для конфигурации игры
 WIDTH = 1200
 HEIGHT = 700
 FPS = 60
 
+# переменные цветовых кодов
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
@@ -16,11 +19,13 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
+# метод для безопасной загрузки в системе
 def resource_path(relative):
     if hasattr(sys, "_MEIPASS"):
         return os.path.join(sys._MEIPASS, relative)
     return os.path.join(relative)
 
+# инициализация элементов движка
 pygame.init()
 pygame.mixer.init()
 pygame.font.init()
@@ -32,10 +37,11 @@ GAME_OVER = False
 GAME_STARTED = False
 start_ticks = 0
 
+# класс игрока
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = player_images[0]
+        self.image = storage.player_images[0]
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2 - 180
         self.rect.bottom = HEIGHT - 120
@@ -48,7 +54,7 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         if self.lives > 0:
-            animation(self, player_images, 50, True)
+            animation(self, storage.player_images, 50, True)
             self.speedx = 0
             keystate = pygame.key.get_pressed()
             if keystate[pygame.K_LEFT]:
@@ -81,7 +87,7 @@ class Player(pygame.sprite.Sprite):
 
     def die(self):
         sound_mixer.sounds["big_expl_sound" + str(random.randint(1, 2))].play(0)
-        animation(self, big_explosion, 30, False)
+        animation(self, storage.big_explosion, 30, False)
 
     def shoot(self):
         bullet = Our_bullet(self.rect.centerx + 20, self.rect.top + 80, -20)
@@ -92,11 +98,11 @@ class Player(pygame.sprite.Sprite):
         all_sprites.add(bullet)
         our_bullets.add(bullet)
 
-
+# класс врага
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, x, attack_speed):
         pygame.sprite.Sprite.__init__(self)
-        self.image = enemy_images[0]
+        self.image = storage.enemy_images[0]
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2 - 180 + x
         self.rect.bottom = HEIGHT - 700
@@ -111,7 +117,7 @@ class Enemy(pygame.sprite.Sprite):
         now = pygame.time.get_ticks()
         self.rect.y += self.speedy
         if self.is_alive:
-            animation(self, enemy_images, 50, True)
+            animation(self, storage.enemy_images, 50, True)
             if now - self.last_shoot_update > self.attack_speed:
                 self.shoot()
                 self.last_shoot_update = now
@@ -119,7 +125,7 @@ class Enemy(pygame.sprite.Sprite):
                 player.lives -= player.lives
                 self.is_alive = False
         else:
-            animation(self, big_explosion, 30, False)
+            animation(self, storage.big_explosion, 30, False)
         for bullet in our_bullets:
             if is_collided_with(self, bullet):
                 sound_mixer.sounds["big_expl_sound"+str(random.randint(1,2))].play(0)
@@ -127,7 +133,6 @@ class Enemy(pygame.sprite.Sprite):
                 break
 
     def die(self):
-        print("Enemy: ", scores_.scores, 1 + spawner.i)
         scores_.scores += 1 + spawner.i
         self.is_alive = False
 
@@ -140,11 +145,11 @@ class Enemy(pygame.sprite.Sprite):
         all_sprites.add(bullet)
         enemy_bullets.add(bullet)
 
-
+# класс пули игрока
 class Our_bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, speed):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.scale(bullet_img, (30, 30))
+        self.image = pygame.transform.scale(storage.bullet_img, (30, 30))
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
@@ -166,13 +171,13 @@ class Our_bullet(pygame.sprite.Sprite):
                 self.is_alive = False
         else:
             our_bullets.remove(self)
-            animation(self, small_explosion, 30, False)
+            animation(self, storage.small_explosion, 30, False)
 
-
+# класс вражеской пули
 class Enemy_bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, speed):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.rotate(pygame.transform.scale(bullet_img, (30, 30)), 180)
+        self.image = pygame.transform.rotate(pygame.transform.scale(storage.bullet_img, (30, 30)), 180)
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
@@ -193,13 +198,13 @@ class Enemy_bullet(pygame.sprite.Sprite):
                 self.is_alive = False
         else:
             enemy_bullets.remove(self)
-            animation(self, small_explosion, 30, False)
+            animation(self, storage.small_explosion, 30, False)
 
-
+# класс для отображения жизней
 class Lives_panel(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = live_image
+        self.image = storage.live_image
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2 - 400
         self.rect.bottom = HEIGHT - 50
@@ -210,7 +215,7 @@ class Lives_panel(pygame.sprite.Sprite):
             screen.blit(self.image, self.rect)
             self.rect.centerx = 0
 
-
+# класс для отображения очков
 class Scores:
     def __init__(self):
         self.scores = 0
@@ -218,10 +223,10 @@ class Scores:
     def update(self):
         print_text("SCORES: " + str(self.scores), 25, WHITE, 150, 100)
 
-
+# класс главного экрана
 class Main_screen:
     def __init__(self):
-        self.image = main_screen_images[0]
+        self.image = storage.main_screen_images[0]
         self.rect = self.image.get_rect()
         self.rect.x += 70
         self.last_sprite_update = pygame.time.get_ticks()
@@ -230,12 +235,12 @@ class Main_screen:
 
     def update(self):
         if self.in_main_screen:
-            animation(self,main_screen_images, 50, True)
+            animation(self,storage.main_screen_images, 50, True)
 
-
+# класс телевизора(анимация ряби, включение и выключение экрана)
 class TV:
     def __init__(self):
-        self.image = tv[0]
+        self.image = storage.tv[0]
         self.rect = self.image.get_rect()
         self.last_sprite_update = pygame.time.get_ticks()
         self.i = 0
@@ -245,21 +250,18 @@ class TV:
 
     def update(self):
         if self.on_animation:
-            #print(self.idle, self.on_animation, self.off_animation)
-            on = animation(self, tv_onoff_images, 30, False)
+            on = animation(self, storage.tv_onoff_images, 30, False)
             if not on is None:
                 self.on_animation = on
                 self.idle = not self.on_animation
         elif self.idle:
-            #print(self.idle, self.on_animation, self.off_animation)
-            animation(self, tv, 200, True)
+            animation(self, storage.tv, 200, True)
         if self.off_animation:
-            #print(self.idle, self.on_animation, self.off_animation)
-            off = animation(self, tv_onoff_images[::-1], 30, False)
+            off = animation(self, storage.tv_onoff_images[::-1], 30, False)
             if not off is None:
                 self.off_animation = off
-                self.image = tv_onoff_images[0]
-                pygame.quit()
+                self.image = storage.tv_onoff_images[0]
+                sys.exit()
 
     def TV_game_start(self):
         main_screen.in_main_screen = False
@@ -284,10 +286,10 @@ class TV:
         self.off_animation = True
 
 
-
+# класс кнопки для рестарта, включения и выключения игры
 class Button:
     def __init__(self, x, y, func1, func2):
-        self.image = button_images[0]
+        self.image = storage.button_images[0]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -296,8 +298,7 @@ class Button:
         self.func2 = func2
 
     def click(self):
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        if self.rect.left < mouse_x < self.rect.left + self.rect.width and self.rect.top < mouse_y < self.rect.top + self.rect.height:
+        if is_pressed_by_mouse(self):
             sound_mixer.sounds["tv_sound_buttons"].play(0)
             self.on = not self.on
             if not self.on:
@@ -310,11 +311,11 @@ class Button:
     def update(self):
         screen.blit(self.image, self.rect)
         if not self.on:
-            self.image = button_images[1]
+            self.image = storage.button_images[1]
         else:
-            self.image = button_images[0]
+            self.image = storage.button_images[0]
 
-
+# класс кнопки регулирования громкости
 class Sound_button:
     def __init__(self, x, y, image, dest):
         self.image = image
@@ -324,8 +325,7 @@ class Sound_button:
         self.dest = dest
 
     def click(self):
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        if self.rect.left < mouse_x < self.rect.left + self.rect.width and self.rect.top < mouse_y < self.rect.top + self.rect.height:
+        if is_pressed_by_mouse(self):
             sound_mixer.sounds["tv_sound_buttons"].play(0)
             if self.dest == "+":
                 sound_mixer.volume += 0.01
@@ -335,7 +335,7 @@ class Sound_button:
     def update(self):
         screen.blit(self.image, self.rect)
 
-
+# класс, который реализовывает создание врагов, усложнения игры
 class Spawner:
     def __init__(self):
         self.speed = 1400
@@ -354,9 +354,7 @@ class Spawner:
             self.i += 1
             self.difficulty += self.difficulty / self.i
 
-    def get_i(self):
-        return self.i
-
+# класс для загрузки, хранения и воспроизведения звуков
 class Sound_mixer():
     def __init__(self):
         self.volume = 0.1
@@ -376,7 +374,25 @@ class Sound_mixer():
         mixer.music.set_volume(self.volume)
         for sound in self.sounds.keys():
             self.sounds[sound].set_volume(self.volume)
-
+# класс для загрузки всех спрайтов
+class Storage:
+    def __init__(self):
+        self.player_images = load_images("our_plane", 3, 120)
+        self.enemy_images = load_images("enemy_plane", 3, 120)
+        self.small_explosion = load_images("small_explosion", 9, 50)
+        self.big_explosion = load_images("big_explosion", 9, 120)
+        self.button_images = load_images("button_pos", 2, 110)
+        self.button_sound_images = load_images("button_sound", 2, 120)
+        self.main_screen_images = load_images("main_screen", 38, 0)
+        self.live_image = pygame.image.load(resource_path(os.path.join("venv\\Sprites\\", "our_plane_lives.png"))).convert_alpha()
+        self.live_image.set_alpha(200)
+        self.tv = load_images("TV_", 3, 0)
+        self.tv_onoff_images = load_images("TV_onoff", 11, 0)
+        self.bullet_img = pygame.image.load(resource_path(os.path.join("venv\\Sprites\\","bullet.png"))).convert_alpha()
+        self.background = self.main_screen_images[0]
+        self.background_rect = self.background.get_rect()
+        self.background_rect.x += 100
+# метод для проигрывания анимации
 def animation(Entity, images, speed, endless):
     now = pygame.time.get_ticks()
     is_alive = True
@@ -402,7 +418,11 @@ def animation(Entity, images, speed, endless):
 def is_collided_with(self, sprite):
     return self.rect.colliderect(sprite.rect)
 
+def is_pressed_by_mouse(Entity):
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    return Entity.rect.left < mouse_x < Entity.rect.left + Entity.rect.width and Entity.rect.top < mouse_y < Entity.rect.top + Entity.rect.height
 
+# метод для загрузки нескольких спрайтов в массив
 def load_images(name, count, scale):
     array = []
     if scale == 0:
@@ -421,28 +441,14 @@ def load_images(name, count, scale):
 
     return array
 
-
+# метод для отображения текста на экране
 def print_text(text, size, color, x, y):
     font = pygame.font.Font(resource_path(os.path.join("venv\\Fonts\\","MinecraftFont.ttf")), size)
     screen.blit(font.render(text, True, color), (x, y))
 
 
-player_images = load_images("our_plane", 3, 120)
-enemy_images = load_images("enemy_plane", 3, 120)
-small_explosion = load_images("small_explosion", 9, 50)
-big_explosion = load_images("big_explosion", 9, 120)
-button_images = load_images("button_pos", 2, 110)
-button_sound_images = load_images("button_sound", 2, 120)
-main_screen_images = load_images("main_screen", 38, 0)
-live_image = pygame.image.load(resource_path(os.path.join("venv\\Sprites\\", "our_plane_lives.png"))).convert_alpha()
-live_image.set_alpha(200)
-tv = load_images("TV_", 3, 0)
-tv_onoff_images = load_images("TV_onoff", 11, 0)
-bullet_img = pygame.image.load(resource_path(os.path.join("venv\\Sprites\\","bullet.png"))).convert_alpha()
-background = main_screen_images[0]
-background_rect = background.get_rect()
-background_rect.x += 100
-
+# создание обьектов и распределение их по группам
+storage = Storage()
 all_sprites = pygame.sprite.Group()
 our_bullets = pygame.sprite.Group()
 enemy_bullets = pygame.sprite.Group()
@@ -454,80 +460,98 @@ scores_ = Scores()
 lives_panel = Lives_panel()
 Tv = TV()
 main_screen = Main_screen()
+
+# создание кнопок меню
 start_button = Button(945, 100, Tv.TV_game_start, Tv.TV_back_to_main)
 exit_button = Button(945, 250, Tv.TV_off, Tv.TV_on)
-sound_up_button = Sound_button(818, 95, button_sound_images[0], "+")
-sound_down_button = Sound_button(818, 205, button_sound_images[1], "-")
-
+sound_up_button = Sound_button(818, 95, storage.button_sound_images[0], "+")
+sound_down_button = Sound_button(818, 205, storage.button_sound_images[1], "-")
+# загрузка фоновой музыки
 mixer.music.load(resource_path(os.path.join("venv\\Sounds\\","bg_music.mp3")))
 mixer.music.set_volume(0.2)
 mixer.music.play(-1)
 
+# переменная от которой зависит цикл игры
 running = True
 
 while running:
+    # установка определенное колличество кадров в секунду (в нашем случае 60)
     clock.tick(FPS)
+    # цикл для прослушивания всех событий в игре
     for event in pygame.event.get():
-        sound_mixer.update()
-
+        # если игрок закрывает окно, то прекратить цикл
         if event.type == pygame.QUIT:
             running = False
+        # если игрок нажал клавишу мыши
         if event.type == pygame.constants.MOUSEBUTTONDOWN:
+            # проверка всех кнопок, нажали ли на них
             start_button.click()
             exit_button.click()
             sound_up_button.click()
             sound_down_button.click()
+        # если нажата кнопка на клавиатуре и игра не на главном экране
         elif event.type == pygame.KEYDOWN and not main_screen.in_main_screen:
+            # если пробел и игра не на паузе, то игрок может стрелять
             if event.key == pygame.K_SPACE and player.lives > 0 and not PAUSED:
                 player.shoot()
+            # если ESCAPE, то поставить игру на паузу
             if event.key == pygame.K_ESCAPE and not GAME_OVER:
                 PAUSED = not PAUSED
+    # если игра не на главном экране
     if not main_screen.in_main_screen:
+        # если игра не была ранее запущена, то запустить
         if not GAME_STARTED:
             Tv.on_animation = True
             all_sprites = pygame.sprite.Group()
             our_bullets = pygame.sprite.Group()
             enemy_bullets = pygame.sprite.Group()
             enemies = pygame.sprite.Group()
-            player = Player()
             spawner = Spawner()
+            player = Player()
+            scores_ = Scores()
             lives_panel = Lives_panel()
             all_sprites.add(player)
             GAME_STARTED = True
-
+        # если у игрока осталось меньше одного здоровья, то игра закончена
         if player.lives < 1 and GAME_OVER == False:
             GAME_OVER = True
             sound_mixer.sounds["game_over_sound"].play(0)
             start_ticks = pygame.time.get_ticks()
-        screen.blit(background, background_rect)
+        # отображение фона
+        screen.blit(storage.background, storage.background_rect)
+        # если игра не на паузе, то создавать новых врагов и обновлять все игровые элементы
         if not PAUSED:
             spawner.update()
             all_sprites.update()
 
+        # отрисовываем игровые елементы(игрока, врагов, пули, взрывы, жизни игрока, очки)
         all_sprites.draw(screen)
         lives_panel.update()
         screen.blit(lives_panel.image, lives_panel.rect)
         scores_.update()
 
+        # если игра на паузе отрисовывать слово PAUSED в углу
         if PAUSED:
             print_text("PAUSE", 50, WHITE, 500, 80)
+        # если игра не на паузе и игра закончена
         elif not PAUSED and GAME_OVER:
+            # отрысовываем GAME OVER в центре экрана и ждать три секунды
             print_text("GAME OVER", 50, WHITE, 250, 300)
             seconds = (pygame.time.get_ticks() - start_ticks) / 1000
+            # когда прошло три секунды, то перейти на главный экран
             if seconds > 3:
                 start_button.on = True
-                GAME_STARTED = False
                 Tv.on_animation = True
                 main_screen.in_main_screen = True
-                GAME_OVER = False
-
+    # если сейчас на главном экране
     if main_screen.in_main_screen:
         main_screen.update()
         screen.blit(main_screen.image, main_screen.rect)
         GAME_STARTED = False
         GAME_OVER = False
-
+    # обновляем пользовательский интерфейс(анимация телевизора, громкость звука, FPS)
     Tv.update()
+    sound_mixer.update()
     screen.blit(Tv.image, Tv.rect)
     print_text(str(int(clock.get_fps())), 10, WHITE, 5,5)
     start_button.update()
